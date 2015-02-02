34303b617d6e76ba17042385bf6c966c1b3396a8
Hussard <adrien.ricartnoblet@gmail.com>
2015-02-02 20:48:06 +0100

    Minor fix in init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ perl -e '
 		"puppet/modules/stdlib" => "https://github.com/puppetlabs/puppetlabs-stdlib.git",
 		"puppet/modules/concat" => "https://github.com/puppetlabs/puppetlabs-concat.git",
 		"puppet/modules/apache" => "https://github.com/puppetlabs/puppetlabs-apache.git",
-		"puppets/modules/staging" => "https://github.com/nanliu/puppet-staging.git",
+		"puppet/modules/staging" => "https://github.com/nanliu/puppet-staging.git",
 		"puppet/modules/mysql" => "https://github.com/puppetlabs/puppetlabs-mysql.git",
 	);
 

08a37bd27d12db31f9c8c7ad2388a1f5578f12b1
Hussard <adrien.ricartnoblet@gmail.com>
2015-02-02 22:06:07 +0100

    Addition of the git clone for Osclass in init.sh

--- init.sh
+++ init.sh
@@ -1,19 +1,23 @@
-#!env bash
+#!/usr/bin/env perl
 
-perl -e '
-	%deps = (
-		"puppet/modules/stdlib" => "https://github.com/puppetlabs/puppetlabs-stdlib.git",
-		"puppet/modules/concat" => "https://github.com/puppetlabs/puppetlabs-concat.git",
-		"puppet/modules/apache" => "https://github.com/puppetlabs/puppetlabs-apache.git",
-		"puppet/modules/staging" => "https://github.com/nanliu/puppet-staging.git",
-		"puppet/modules/mysql" => "https://github.com/puppetlabs/puppetlabs-mysql.git",
-	);
+%deps = (
+	"puppet/modules/stdlib" => "https://github.com/puppetlabs/puppetlabs-stdlib.git",
+	"puppet/modules/concat" => "https://github.com/puppetlabs/puppetlabs-concat.git",
+	"puppet/modules/apache" => "https://github.com/puppetlabs/puppetlabs-apache.git",
+	"puppet/modules/staging" => "https://github.com/nanliu/puppet-staging.git",
+	"puppet/modules/mysql" => "https://github.com/puppetlabs/puppetlabs-mysql.git",
+);
 
-	for my $k (keys(%deps)) {
-		if(! -d $k) {
-			`git clone $deps{$k} $k`
-		} else {
+for my $k (keys(%deps)) {
+	if(! -d $k) {
+		`git clone $deps{$k} $k`
+	} else {
 
-		}
 	}
-'
+}
+
+if(! -d "sites/osclass") {
+	`git clone -b v3.5.3 https://github.com/osclass/Osclass.git sites/osclass`
+}
+
+1;

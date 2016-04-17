44aed6e388a113a807529c05568346106c53d1d1
Hussard <adrien.ricartnoblet@gmail.com>
2016-04-17 20:59:17 +0200

    Remove bash customization from Puppet provising

--- puppet/modules/baseconfig/manifests/init.pp
+++ puppet/modules/baseconfig/manifests/init.pp
@@ -7,29 +7,6 @@ class baseconfig {
     command => '/usr/bin/apt-get update';
   }
 
-  # host { 'hostmachine':
-  #   ip => '192.168.0.1';
-  # }
-
-  file {
-    '/home/vagrant/.bash':
-    ensure => directory
-  }
-
-  file {
-    '/home/vagrant/.bash/git-aware-prompt':
-    recurse => true,
-    source => "puppet:///modules/baseconfig/git-aware-prompt"
-  }
-
-  file {
-    '/home/vagrant/.bash_profile':
-    owner => 'vagrant',
-    group => 'vagrant',
-    mode  => '0644',
-    source => 'puppet:///modules/baseconfig/bash_profile';
-  }
-
   package {[
       'git',
       'emacs',
